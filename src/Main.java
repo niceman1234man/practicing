2f60667b383912b3df82ffc6b642a840a9186d52
Yihunie Tarekegn <136583776+niceman1234man@users.noreply.github.com>
2024-06-02 11:04:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
